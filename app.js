3f524f5dff4c0a5cbc8e29944b42117b1f6ab987
waleolajumoke <waleolajumoke@gmail.com>
2026-07-25 14:17:01 +0100

    edited jenkinsfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("welcome to Tech365");
+  res.send("welcome to Tech365. v2");
 });
 
 app.get("/health", (req, res) => {
